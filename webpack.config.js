4b4171474307831d33c97d87d3fc1a1edee93b13
James Barford-Evans <jamesbarfordevans@gmail.com>
2019-01-05 16:02:53 +0000

    added html, clean plugin and webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: "./src/App/index.tsx",
@@ -7,9 +9,12 @@ module.exports = {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, 'dist'),
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
-    extensions: ['.js', '.json', '.ts', '.tsx'],
+    extensions: [".js", ".json", ".ts", ".tsx"],
+  },
+  devServer: {
+    contentBase: './dist'
   },
   module: {
     rules: [
@@ -30,6 +35,11 @@ module.exports = {
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: 'style.css' })
+    new MiniCssExtractPlugin({ filename: "style.css" }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: "Webpack React Typescript Sandbox",
+      template: 'template.html'
+    })
   ]
 };
