8321d5c4850d93b36155a345cdfc74325d3f7220
James Barford-Evans <jamesbarfordevans@gmail.com>
2019-01-05 14:48:35 +0000

    minimal change

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
 
-const App = () => <div>hello world</div>;
+const App = () => <div>hello</div>;
 
 render( <App />, document.getElementById("root") );
